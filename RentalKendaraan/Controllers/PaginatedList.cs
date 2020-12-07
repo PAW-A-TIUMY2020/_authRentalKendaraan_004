2b004225bcca787cc218411023e532a8ea88cf5d
Nanda yudha pratama <nanda.yudha.ft18@mail.umy.ac.id>
2020-12-07 09:57:43 +0700

    Add project files.

---
+++ RentalKendaraan/Controllers/PaginatedList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using RentalKendaraan_004.Models;
+
+namespace RentalKendaraan_004.Controllers
+{
+    internal class PaginatedList<T>
+    {
+        internal static Task<string> CreateAsync<TEntity>(IQueryable<TEntity> queryable, int v, int pageSize) where TEntity : class
+        {
+            throw new NotImplementedException();
+        }
+
+        internal static Task<string> CreateAsync(IQueryable<Kendaraan> queryable, int v, object pageSize)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RentalKendaraan/Models/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace RentalKendaraan_004.Models
+{
+    public partial class Customer
+    {
+        public int IdCustomer { get; set; }
+
+        [Required(ErrorMessage = "Nama Customer Tidak Boleh Kosong")]
+        public string NamaCustomer { get; set; }
+
+        [RegularExpression("^[0-9]*$", ErrorMessage = "Hanya Boleh Diisi Oleh Angka")]
+        public string Nik { get; set; }
+
+        [Required(ErrorMessage = "Alamat Tidak Boleh Kosong")]
+        public string Alamat { get; set; }
+
+        [MinLength(10, ErrorMessage = "No HP minimal 10 angka")]
+        [MaxLength(13, ErrorMessage = "No HP maksimal 13 angka")]
+        public string NoHp { get; set; }
+
+        public int? IdGender { get; set; }
+        public object IdGenderNavigation { get; internal set; }
+    }
+}
